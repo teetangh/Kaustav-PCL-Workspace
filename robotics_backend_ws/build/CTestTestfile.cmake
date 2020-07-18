007573204d07a14f0c3ebcb001bf6107965e9bdd
Kaustav Ghosh <teetangh@gmail.com>
2020-07-18 10:52:05 +0530

    catkin_make empty

---
+++ robotics_backend_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/kaustav/Desktop/Kaustav-ROS-practice/robotics_backend_ws/src
+# Build directory: /home/kaustav/Desktop/Kaustav-ROS-practice/robotics_backend_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("driver_wrapper_python_pkg")
---
+++ robotics_backend_ws/build/driver_wrapper_python_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kaustav/Desktop/Kaustav-ROS-practice/robotics_backend_ws/src/driver_wrapper_python_pkg
+# Build directory: /home/kaustav/Desktop/Kaustav-ROS-practice/robotics_backend_ws/build/driver_wrapper_python_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
